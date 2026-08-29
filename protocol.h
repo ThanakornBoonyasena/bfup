e7a07aa1b4563ce63b42ce3f67afb2ae454dbd78
thanakorn boonyasena <thanakorn.boonyasena@gmail.com>
2026-08-30 00:49:22 +0700

    fix and comment things

--- protocol.h
+++ protocol.h
@@ -7,17 +7,17 @@ struct __attribute__((packed)) bfup_payload {
 	uint8_t version_n_type; // first 4 bytes for version and the rest for msg type
 	uint16_t data_len;
 	uint8_t data[];
-}
+};
 
 void putInfo(uint8_t **ptr, uint16_t len, const void *data);
 
-bfup_payload *mkPlay(int fd, char *target_dr);
-bfup_payload *mkEmpty(uint8_t type); // this shit doesnt really do anything just set the type field for START, END, DONE
-bfup_payload *mkRule(enum Rule rules);
-bfup_payload *mkNo(char *msg);
-bfup_payload *mkWeapon(enum Rule weapon);
-bfup_payload *mkResult(uint8_t result);
-bfup_payload *mkContent(char *content);
+struct bfup_payload *mkPlay(int fd, char* filename, char *target_dr);
+struct bbfup_payload *mkEmpty(uint8_t type); // this shit doesnt really do anything just set the type field for START, END, DONE
+//struct bbfup_payload *mkRule(enum Rule rules);
+struct bbfup_payload *mkNo(char *msg);
+//struct bbfup_payload *mkWeapon(enum Rule weapon);
+struct bbfup_payload *mkResult(uint8_t result);
+struct bbfup_payload *mkContent(char *content);
 
 #endif
 
